fbc0b1a76a1b7e54a5f4a0e1824f6873f4e2819c
rolemberg <vinnie.nsts@gmail.com>
2024-02-29 23:24:42 -0300

    restful api completed

--- src/main/java/com/rest/app/api/Controllers/Home.java
+++ src/main/java/com/rest/app/api/Controllers/Home.java
@@ -1,15 +1,53 @@
 package com.rest.app.api.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.rest.app.api.Models.Workers;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.rest.app.api.Repo.WorkerRepo;
+
+import java.util.List;
 
 @RestController
 public class Home {
 
+    @Autowired
+    private WorkerRepo workerRepo;
+
     @GetMapping(value = "/")
-    public String home() {
+    public String gatHome() {
       return "Olá novamente!";
     };
+
+    @GetMapping(value = "/api/workers")
+    public List<Workers> getWorkers() {
+        return workerRepo.findAll();
+    };
+
+    @PostMapping(value = "/api/workers/create")
+    public String saveWorker(@RequestBody Workers workers) {
+        workerRepo.save(workers);
+        return "status: ok";
+    };
+
+    @PutMapping(value = "/api/update/{id}")
+    public String update(@PathVariable long id, @RequestBody Workers workers) {
+        Workers updateWorker = workerRepo.findById(id).get();
+        updateWorker.setNome(workers.getNome());
+        updateWorker.setSobrenome(workers.getSobrenome());
+        updateWorker.setIdade(workers.getIdade());
+        updateWorker.setCargo(workers.getCargo());
+        workerRepo.save(updateWorker);
+        return "updated";
+    };
+
+    @DeleteMapping(value = "/api/delete/{id}")
+    public String delete(@PathVariable long id, @RequestBody Workers workers) {
+        workerRepo.deleteById(id);
+        return "sucesfully deleted";
+    };
+
 }
 
 
--- src/main/java/com/rest/app/api/Repo/WorkerRepo.java
+++ src/main/java/com/rest/app/api/Repo/WorkerRepo.java
@@ -2,6 +2,8 @@ package com.rest.app.api.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.rest.app.api.Models.Workers;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkerRepo extends JpaRepository<Workers, Long> {
 }
